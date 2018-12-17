163d257a288e60290c1db375abf3a96909147660
Antoine Mikael <33396875+Mikeyouf@users.noreply.github.com>
2018-12-17 22:34:40 +0100

    Ajout d'aides

---
+++ precache-manifest.bc24b35fe7ccbca5fe95ae8720dfb3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d05cc4540f591453909",
+    "url": "/static/js/main.3d05cc45.chunk.js"
+  },
+  {
+    "revision": "0909f6ca0bc3d4c2cbaa",
+    "url": "/static/js/1.0909f6ca.chunk.js"
+  },
+  {
+    "revision": "3d05cc4540f591453909",
+    "url": "/static/css/main.71b915ac.chunk.css"
+  },
+  {
+    "revision": "0909f6ca0bc3d4c2cbaa",
+    "url": "/static/css/1.492fc0f1.chunk.css"
+  },
+  {
+    "revision": "efe725727097e74185fa8f52e64e4435",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bbb4f465973082fc2f0e7d24426c5cdd.js"
+  "/precache-manifest.bc24b35fe7ccbca5fe95ae8720dfb3b1.js"
 );
 
 workbox.clientsClaim();
